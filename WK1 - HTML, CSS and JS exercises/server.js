4377d94969cf919ad51dd99bc3179524572fd349
Ricool06 <ricoolohsix@hotmail.com>
2018-10-10 20:21:03 +0100

    Create simple node server

---
+++ WK1 - HTML, CSS and JS exercises/server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(request, response) {
+  var parsedUrl = url.parse(request.url, true);
+
+  console.log(parsedUrl.query);
+
+  response.end(
+    '<p style=color:'
+    + parsedUrl.pathname.substr(1)
+    + '>'
+    + parsedUrl.pathname
+    + ' params: '
+    + JSON.stringify(parsedUrl.query)
+    + '</p>'
+  );
+}).listen(8080);
